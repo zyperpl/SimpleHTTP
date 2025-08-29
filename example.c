832c58b301901a3eecadf9a2dd63941992678334
Kacper Zybała <zyperpl@gmail.com>
2025-08-29 21:42:43 +0200

    Add usage example

---
+++ example.c
@@ -0,0 +1,36 @@
+#include "server.h"
+#include <stdio.h>
+
+void endpoint_hello(Client *client)
+{
+  server_response(client, 200, "Hello, World!\n");
+}
+
+void endpoint_value(Client *client)
+{
+  if (client->values.count <= 0)
+  {
+    server_response(client, 400, "Bad Request\n");
+    return;
+  }
+
+  if (client->values.arr[0].type != SERVER_VALUE_INT)
+  {
+    server_response(client, 400, "Bad Request\n");
+    return;
+  }
+
+  int val = client->values.arr[0].value.i * 2;
+  char response[64];
+  sprintf(response, "Value doubled: %d\n", val);
+  server_response(client, 200, response);
+}
+
+int main(void)
+{
+  Server server;
+  server_init(&server, 8000);
+  server_endpoint(&server, "/hello", endpoint_hello);
+  server_endpoint(&server, "/value/%d", endpoint_value);
+  return server_run(&server);
+}
